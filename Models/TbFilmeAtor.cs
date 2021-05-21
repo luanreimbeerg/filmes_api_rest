a589a4730d9937b82cd2a8968a89977bcbf3b2e2
luanreimbeerg <luanreimberg@gmail.com>
2021-05-21 10:19:15 -0300

    mapeamento banco feito

---
+++ Models/TbFilmeAtor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace filmes_api_rest.Models
+{
+    [Table("tb_filme_ator")]
+    public partial class TbFilmeAtor
+    {
+        [Key]
+        [Column("id_filme_ator")]
+        public int IdFilmeAtor { get; set; }
+        [Column("nm_personagem", TypeName = "varchar(100)")]
+        public string NmPersonagem { get; set; }
+        [Column("id_filme")]
+        public int? IdFilme { get; set; }
+        [Column("id_ator")]
+        public int? IdAtor { get; set; }
+
+        [ForeignKey(nameof(IdAtor))]
+        [InverseProperty(nameof(TbAtor.TbFilmeAtor))]
+        public virtual TbAtor IdAtorNavigation { get; set; }
+        [ForeignKey(nameof(IdFilme))]
+        [InverseProperty(nameof(TbFilme.TbFilmeAtor))]
+        public virtual TbFilme IdFilmeNavigation { get; set; }
+    }
+}
